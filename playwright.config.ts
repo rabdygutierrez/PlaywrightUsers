b98467b7661aa1ee76d05afb6b75d6d20eaddc6a
Rabdy Gutierrez <rabdygutierrez@gmail.com>
2025-06-26 16:01:48 -0400

    update variable

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. */
   use: {
-    headless: process.env.HEADLESS !== 'false', // Controlar modo headless con variable de entorno
+    headless: true, // ejecutar sin interfaz gráfica
     ignoreHTTPSErrors: true,
     /* Desactivar sandbox y otras flags para entornos Linux headless */
     launchOptions: {
